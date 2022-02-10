d409092c9347207e6b91cc90a3203b74176bfb84
mendozaremi <mendozaremi@gmail.com>
2022-02-09 21:00:02 -0500

    Revert "bootcamp recyclerview & fragments"
    This reverts commit d9a21d3682c1035ffb960dcf030acf447a3c4f9e.

--- app/src/main/java/com/Phase4/pathways/views/MainActivity.kt
+++ app/src/main/java/com/Phase4/pathways/views/MainActivity.kt
@@ -2,23 +2,12 @@ package com.Phase4.pathways.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.RecyclerView
 import com.Phase4.pathways.R
-import com.Phase4.pathways.adapter.BootcampAdapter
-import com.Phase4.pathways.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        //initializing data
-        val myDataset = Datasource().loadBootcamps()
-        var recylclerView = findViewById<RecyclerView>(R.id.recycler_view)
-
-        recylclerView.adapter = BootcampAdapter(this,myDataset)
-        recylclerView.setHasFixedSize(true)
     }
 }
 
