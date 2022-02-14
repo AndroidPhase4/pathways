18cfd5ffa51f273ab29e67bd59e012fe9355c83d
zippotle <zipporahnajar@gmail.com>
2022-02-14 11:29:20 -0500

    initail commit for job api

---
+++ app/src/main/java/com/Phase4/pathways/model/Job.kt
@@ -0,0 +1,11 @@
+package com.Phase4.pathways.model
+
+data class Job {
+    //Data we will retreive from the API
+    //Considering adding the jobUrl
+    val employerName : String = "",
+    val jobTitle : String = "",
+    val locationName : String = "",
+    val jobDescription : String = "",
+    val jobUrl : String = ""
+}
