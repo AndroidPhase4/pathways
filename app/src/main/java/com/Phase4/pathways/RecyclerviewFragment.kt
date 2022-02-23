658c43e91cdb308d1ac26e1d5bb91bb9373e4a49
Flowersa29 <flowersa29@gmail.com>
2022-02-23 10:09:08 -0600

    third commit no errors

--- app/src/main/java/com/Phase4/pathways/RecyclerviewFragment.kt
+++ app/src/main/java/com/Phase4/pathways/RecyclerviewFragment.kt
@@ -5,18 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.Phase4.pathways.model.Project
 import com.Phase4.pathways.model.ProjectAdapter
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class RecyclerviewFragment : Fragment() {
 
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>? = null
-
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
+    val title: List<Project> = ArrayList()
 
 
 
@@ -26,16 +28,31 @@ class RecyclerviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recyclerview, container, false)
+        val view = inflater.inflate(R.layout.fragment_recyclerview, container, false)
+
+        val title = view.findViewById<TextView>(R.id.text_view_project_title)
+        val description = view.findViewById<TextView>(R.id.text_view_project_desc)
+
+        initRecyclerView(view)
+        return view
 
     }
+
+    private fun initRecyclerView(view: View){
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        recyclerView.adapter = adapter
+    }
+
+
 
     override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
         super.onViewCreated(itemView, savedInstanceState)
-        adapter = ProjectAdapter(Project("test", "test", 12))
+
 
         }
-    }
+
 
 
 
