dd8b3edb1e6b27de7eb0917514748d21644d3f8e
Amantina Sosa <amantinasosa@gmail.com>
2022-02-22 21:53:09 -0500

    update

--- app/src/main/java/com/Phase4/pathways/views/Fragment_bootcamp.kt
+++ app/src/main/java/com/Phase4/pathways/views/Fragment_bootcamp.kt
@@ -5,52 +5,80 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.RecyclerView
 import com.Phase4.pathways.R
 import com.Phase4.pathways.adapter.BootcampAdapter
 import com.Phase4.pathways.data.Datasource
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-class Fragment_bootcamp : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
+class Fragment_bootcamp : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.bootcamp_list)
+        setContentView(R.layout.activity_main)
 
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.activity_main_navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
+//        setupActionBarWithNavController(navController)
+        //initializing data == test location
+        val myDataset = Datasource().loadBootcamps()
+        var recylclerView = findViewById<RecyclerView>(R.id.recycler_view)
+
+        recylclerView.adapter = BootcampAdapter(this,myDataset)
+        recylclerView.setHasFixedSize(true)
     }
+}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bootcamp, container, false)
-    }
-
-    companion object {
-
-        fun newInstance(param1: String, param2: String) =
-            Fragment_bootcamp().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+//
+//class Fragment_bootcamp : Fragment() {
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.bootcamp_list)
+////        setContentView(R.layout.bootcamp_list)
+////
+////
+////        //initializing data == test location
+////        val myDataset = Datasource().loadBootcamps()
+////        var recylclerView = findViewById<RecyclerView>(R.id.recycler_view)
+////
+////        recylclerView.adapter = BootcampAdapter(this,myDataset)
+////        recylclerView.setHasFixedSize(true)
+//
+//
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//
 //    }
-}
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_bootcamp, container, false)
+//    }
+//
+//    companion object {
+//
+//        fun newInstance(param1: String, param2: String) =
+//            Fragment_bootcamp().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+//}
--- app/src/main/java/com/Phase4/pathways/views/Fragment_web_developer_details.kt
+++ app/src/main/java/com/Phase4/pathways/views/Fragment_web_developer_details.kt
@@ -1,60 +1,39 @@
 package com.Phase4.pathways.views
 
-import android.content.Context
-import android.content.Intent
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.navigation.Navigation
 import com.Phase4.pathways.R
 
 
 class fragment_web_developer_details : Fragment() {
-    private val loggedInUserTextView: TextView? = null
-    private var logOutButton: Button? = null
+//    private val loggedInUserTextView: TextView? = null
+//    private var logOutButton: Button? = null
+    private var bootcampBtn: Button? = null
 
-    //New Addition
-//    private var bootcampBtn: Button? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_web_developer_details, container, false)
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val view = inflater.inflate(R.layout.fragment_web_developer_details, container, false)
-//
-//        logOutButton = view.findViewById<Button>(R.id.fragment_loggedin_logOut)
-//       val bootcampBtn = view.findViewById<Button>(R.id.BootcampBtn)
-//
-//
-////        logOutButton.setOnClickListener(View.OnClickListener { loggedInViewModel.logOut() })
-//
-////        bootcampBtn = setOnClickListener(View.OnClickListener {
-////            Navigation.findNavController(
-////                requireView()
-////            ).navigate(R.id.action_fragment_web_developer_details_to_fragment_bootcamp)
-////        })
-//
-//
-//        bootcampBtn.setOnClickListener {
-//            val intent = Intent(this, Fragment_bootcamp::class.java)
-//            startActivity(intent)
-//        }
-//
-//        return view
-//    }
-//override fun onCreateView(
-//    inflater: LayoutInflater,
-//    container: ViewGroup?,
-//    savedInstanceState: Bundle?
-//): View? {
-//    _binding = StopScheduleFragmentBinding.inflate(inflater, container, false)
-//    val view = binding.root
-//    return view
-//}
+        bootcampBtn = view.findViewById(R.id.BootcampBtn)
+
+        bootcampBtn?.setOnClickListener(View.OnClickListener {
+            Navigation.findNavController(
+                requireView()
+            ).navigate(R.id.action_fragment_web_developer_details_to_fragment_bootcamp)
+        })
+
+//        return inflater.inflate(R.layout.fragment_web_developer_details, container, false)
+        return view
+    }
+
 
 }
+
