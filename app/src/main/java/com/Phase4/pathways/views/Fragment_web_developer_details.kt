4f415a263d38580f6f2d46159867c141ba9b55a1
Amantina Sosa <amantinasosa@gmail.com>
2022-03-09 11:00:45 -0500

    button works & directs you to right frag

--- app/src/main/java/com/Phase4/pathways/views/Fragment_web_developer_details.kt
+++ app/src/main/java/com/Phase4/pathways/views/Fragment_web_developer_details.kt
@@ -13,19 +13,29 @@ import com.Phase4.pathways.R
 
 class fragment_web_developer_details : Fragment() {
     private var bootcampBtn: Button? = null
+    private var webdevresourcesBtn: Button? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_web_developer_details, container, false)
+//        val view2 = inflater.inflate(R.layout.fragment_web_developer_resources, container, false)
 
         bootcampBtn = view.findViewById(R.id.BootcampBtn)
+        webdevresourcesBtn = view.findViewById(R.id.ResourcesBtn)
 
         bootcampBtn?.setOnClickListener(View.OnClickListener {
             Navigation.findNavController(
                 requireView()
             ).navigate(R.id.action_fragment_web_developer_details_to_fragment_bootcamp)
+        })
+
+        webdevresourcesBtn?.setOnClickListener(View.OnClickListener {
+            Navigation.findNavController(
+                requireView()
+            ).navigate(R.id.action_fragment_web_developer_details_to_fragment_web_developer_resources)
         })
         return view
     }
