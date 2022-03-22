3a1ed6b25600c47985d18dddc762b581546800a7
Kamal <knajar212@gmail.com>
2022-03-21 22:26:45 -0400

    updated xml files and desing

---
+++ app/src/main/java/com/Phase4/pathways/model/UrlRepository.kt
@@ -0,0 +1,29 @@
+package com.Phase4.pathways.model
+
+class UrlRepository {
+
+    //Web Development
+    val udacity = "https://www.udacity.com/courses/all"
+    val khanAcademy = "https://www.khanacademy.org/computing/computer-programming"
+    val codecademy = "https://www.codecademy.com/learn/paths/web-development"
+
+    //Android Development
+    val androidDeveloperGoogle = "https://developer.android.com/courses"
+    val androidUdacity = "https://www.udacity.com/course/new-android-fundamentals--ud851"
+    val coursera = "https://www.coursera.org/specializations/android-app-development"
+
+
+    //Data Scientist
+    val dataCamp = "https://www.datacamp.com/"
+    val courseraDataScience = "https://www.coursera.org/browse/data-science"
+    val harvard = "https://extension.harvard.edu/academics/professional-graduate-certificates/data-science-certificate"
+
+
+    //Cyber Security
+    val edx = "https://www.edx.org/learn/cybersecurity"
+    val courseraCyberSecurity = "https://www.coursera.org/courses?query=cybersecurity"
+    val codecademyCyberSecurity = "https://www.codecademy.com/learn/introduction-to-cybersecurity"
+
+
+
+}
