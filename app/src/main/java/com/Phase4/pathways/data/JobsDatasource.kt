340c1f6bd4d447573059435aa20f1ce21bfbe597
mendozaremi <mendozaremi@gmail.com>
2022-03-25 13:51:55 -0400

    added images

--- app/src/main/java/com/Phase4/pathways/data/JobsDatasource.kt
+++ app/src/main/java/com/Phase4/pathways/data/JobsDatasource.kt
@@ -7,9 +7,10 @@ class JobsDatasource() {
 
     fun loadJobs(): List<Job> {
         return listOf<Job>(
-            Job(R.string.job1, R.drawable.app_academy, R.string.job1Desc, R.string.job1Url) ,
-            Job(R.string.job2, R.drawable.app_academy, R.string.job2Desc, R.string.job2Url),
-            Job(R.string.job3, R.drawable.app_academy, R.string.job3Desc, R.string.job3Url)
+            Job(R.string.job1, R.drawable.chase, R.string.job1Desc, R.string.job1Url) ,
+            Job(R.string.job2, R.drawable.bloomberg, R.string.job2Desc, R.string.job2Url),
+            Job(R.string.job3, R.drawable.google, R.string.job3Desc, R.string.job3Url),
+            Job(R.string.job4, R.drawable.meta, R.string.job4Desc, R.string.job4Url)
         )
     }
 }
